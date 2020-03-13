73cd94eca1cbdc88a1865f9ef53508641f8fa242
Aditya Bagad <abagad@abagad.pnq.csb>
2020-03-13 19:52:34 +0530

    Added Database Models

--- todo_project/todo_project/models.py
+++ todo_project/todo_project/models.py
@@ -5,8 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    
+    posts = db.relationship('Task', backref='author', lazy=True)
+
     def __repr__(self):
         return f"User('{self.username}')"
 
