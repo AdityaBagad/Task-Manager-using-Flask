be95e81afbcb5093a3051b0d6f58cfa2e0667017
AdityaBagad <adityabagad@gmail.com>
2020-03-20 19:32:09 +0530

    Display All Tasks and Some Modifications

--- todo_project/todo_project/routes.py
+++ todo_project/todo_project/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request, abort
+from flask import render_template, url_for, flash, redirect
 
 from todo_project import app, db, bcrypt
 
